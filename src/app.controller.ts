9ac098ce71ea2db4450f61567870f9898dc014be
Renan Augusto <renan.augusto18@hotmail.com>
2024-03-01 17:54:02 -0300

    chore: change healthcheck url

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,7 +3,7 @@ import { ApiOperation } from '@nestjs/swagger';
 
 @Controller()
 export class AppController {
-  @Get('/healthcheck')
+  @Get('/')
   @ApiOperation({ summary: 'Check app health', tags: ['Health Check'] })
   healthCheck(): string {
     return 'ok';
