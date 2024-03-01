1603b7d460c1c583902e953f5cf5ab59f02177d9
Renan Augusto <renan.augusto18@hotmail.com>
2024-03-01 16:25:07 -0300

    chore: remove unnecessary workflow and set app port

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const appPort = process.env.APP_PORT || 3000
 
   const config = new DocumentBuilder()
     .setTitle('Financial Management')
@@ -14,6 +15,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+  await app.listen(appPort);
 }
 bootstrap();
