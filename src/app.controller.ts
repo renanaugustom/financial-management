f1de5cbca9cb629f8dee1be98abeedd4a3aa0be9
Renan Augusto <renan.augusto18@hotmail.com>
2024-07-03 09:09:55 -0300

    fix: make health check public

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
 import { ApiOperation } from '@nestjs/swagger';
+import { Public } from '@src/auth/auth.guard';
 
 @Controller()
 export class AppController {
   @Get('/')
   @ApiOperation({ summary: 'Check app health', tags: ['Health Check'] })
+  @Public()
   healthCheck(): string {
     return 'ok';
   }
