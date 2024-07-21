2c26b320541f6350d4a505bb5bdefa794a687b59
Renan Augusto <renan.augusto18@hotmail.com>
2024-07-20 21:53:16 -0300

    docs: add auth swagger doc

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -4,12 +4,28 @@ import { AuthService } from '@src/auth/auth.service';
 import { LoginDTO } from '@src/auth/dtos/login.dto';
 import { LoginResponseDTO } from '@src/auth/dtos/login-response.dto';
 import { Public } from '@src/auth/auth.guard';
+import {
+  ApiInternalServerErrorResponse,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
+import {
+  InternalServerErrorDocsDTO,
+  UserNotAuthorizedDocsDTO,
+} from '@src/docs/dtos/docs.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('login')
+  @ApiUnauthorizedResponse({
+    description: 'Unauthorized',
+    type: UserNotAuthorizedDocsDTO,
+  })
+  @ApiInternalServerErrorResponse({
+    description: 'Internal server error',
+    type: InternalServerErrorDocsDTO,
+  })
   @Public()
   async login(@Body() loginDto: LoginDTO): Promise<LoginResponseDTO> {
     return await this.authService.signIn(loginDto.email, loginDto.password);
