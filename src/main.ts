b1711c3339868c75db03022b50ba9a1fbfbae8c6
Renan Augusto <renan.augusto18@hotmail.com>
2024-08-04 10:12:40 -0300

    fix: disable swagger docs in production

--- src/main.ts
+++ src/main.ts
@@ -7,16 +7,18 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const appPort = process.env.APP_PORT || 3000;
 
-  const config = new DocumentBuilder()
-    .setTitle('Financial Management')
-    .setDescription('Financial Management API')
-    .setVersion('1.0')
-    .build();
-
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
-
   app.useGlobalPipes(new ValidationPipe());
+
+  if (process.env.NODE_ENV !== 'production') {
+    const config = new DocumentBuilder()
+      .setTitle('Financial Management')
+      .setDescription('Financial Management API')
+      .setVersion('1.0')
+      .build();
+
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
+  }
 
   await app.listen(appPort);
 }
